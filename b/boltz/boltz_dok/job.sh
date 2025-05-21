84a4bb4bab8e1a44d1ad07edf5fd7fc8cd0ad460
RongjunG <rongjungao0812@gmail.com>
2025-05-21 15:23:12 +0900

    fix: directory of output

--- b/boltz/boltz_dok/job.sh
+++ b/boltz/boltz_dok/job.sh
@@ -14,14 +14,10 @@ echo "Run boltz calculation ..."
 python3 -m boltz.main predict prot_no_msa.yaml
 
 # Create results directory
-mkdir -p /opt/artifact/boltz_results/prot_no_msa/
-cp prot_no_msa_model_0.cif /opt/artifact/boltz_results/prot_no_msa/
-cp confidence_prot_no_msa_model_0.json /opt/artifact/boltz_results/prot_no_msa/
-#cp pae_prot_no_msa_model_0.npz /opt/artifact/boltz_results/prot_no_msa/
-#cp pde_prot_no_msa_model_0.npz /opt/artifact/boltz_results/prot_no_msa/
-cp plddt_prot_no_msa_model_0.npz /opt/artifact/boltz_results/prot_no_msa/
-cp prot_no_msa.npz /opt/artifact/boltz_results/prot_no_msa/
-#cp prot_no_msa_model_diffusion_samples-1.cif /opt/artifact/boltz_results/prot_no_msa/
+mkdir -p /opt/artifact/boltz_results_prot_no_msa
+cp /boltz_results_prot_no_msa/predictions/prot_no_msa/prot_no_msa_model_0.cif /opt/artifact/boltz_results_prot_no_msa/
+cp /boltz_results_prot_no_msa/predictions/prot_no_msa/confidence_prot_no_msa_model_0.json /opt/artifact/boltz_results_prot_no_msa/
+cp /boltz_results_prot_no_msa/predictions/prot_no_msa/plddt_prot_no_msa_model_0.npz /opt/artifact/boltz_results_prot_no_msa/
 
 echo "Prediction completed. Results are available in results folder."
-ls /opt/artifact/boltz_results/prot_no_msa/
+ls /opt/artifact/boltz_results_prot_no_msa/
