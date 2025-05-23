47f193a6bde7c69961658c7fcb58865503941c3c
RongjunG <rongjungao0812@gmail.com>
2025-05-23 14:58:54 +0900

    fix: links in job_dok.sh and @job.toml for gromacs

--- g/gromacs/lysozyme/job_dok.sh
+++ g/gromacs/lysozyme/job_dok.sh
@@ -4,13 +4,13 @@
 
 
 echo "Downloading input files ..."
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-fix-gromacs/g/gromacs/lysozyme/job.sh
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-fix-gromacs/g/gromacs/lysozyme/ions.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-fix-gromacs/g/gromacs/lysozyme/md.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-fix-gromacs/g/gromacs/lysozyme/mdout.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-fix-gromacs/g/gromacs/lysozyme/minim.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-fix-gromacs/g/gromacs/lysozyme/npt.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-fix-gromacs/g/gromacs/lysozyme/nvt.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/lysozyme/job.sh
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/lysozyme/ions.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/lysozyme/md.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/lysozyme/mdout.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/lysozyme/minim.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/lysozyme/npt.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/lysozyme/nvt.mdp
 echo ""
 
 echo "Running the job ..."
@@ -19,5 +19,5 @@ sh job.sh
 echo "Copy the output files into the DOK artifact"
 mkdir -p /opt/artifact/gromacs_result
 cp ./potential.xvg /opt/artifact/gromacs_result/
-cp ./*.trr /opt/artifact/
+cp ./*.* /opt/artifact/
 ls /opt/artifact/gromacs_result/
