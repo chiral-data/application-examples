1601c141c34e2bd5a3929bd2dae21be2357a1780
Qin Wan <rogerwq@gmail.com>
2025-05-27 18:48:47 +0900

    fix: mkdir -p

--- g/gromacs/protein_ligand/job_dok.sh
+++ g/gromacs/protein_ligand/job_dok.sh
@@ -3,9 +3,9 @@
 #
 
 echo "Downloading new workflow file with use_gpu ..."
-mkdir /app && cd /app
+mkdir -p /app && cd /app
 wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/app/workflow.py
-mkdir /data && cd /data
+mkdir -p /data && cd /data
 wget https://github.com/chiral-data/application-examples/blob/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/data/ions.pdb
 wget https://github.com/chiral-data/application-examples/blob/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/data/ligand.gro
 wget https://github.com/chiral-data/application-examples/blob/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/data/ligand.itp
