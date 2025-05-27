172440321de7f516e9525234275d580be9cf6fcc
Qin Wan <rogerwq@gmail.com>
2025-05-27 18:20:35 +0900

    fix: conda env name

--- g/gromacs/protein_ligand/job.sh
+++ g/gromacs/protein_ligand/job.sh
@@ -2,4 +2,5 @@
 #
 #
 
-conda run --no-capture-output -n complex_setup_env python /app/workflow.py --config /data/workflow.yml
+# conda run --no-capture-output -n complex_setup_env python /app/workflow.py --config /data/workflow.yml
+conda run --no-capture-output -n biobb_wf_protein-complex_md_setup python /app/workflow.py --config /data/workflow.yml
