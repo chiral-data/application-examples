88458b74945a8fdb9df0a9eb43f3e7b0e47f5419
Qin Wan <rogerwq@gmail.com>
2025-05-27 12:17:30 +0900

    fix: run_protein.sh

--- m/myPresto/sievgene/run_protein.sh
+++ m/myPresto/sievgene/run_protein.sh
@@ -4,19 +4,19 @@
 
 PROTEIN="4HP0"
 
-echo "Downloading input files ..."
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-myPresto/m/myPresto/sievgene/job_$PROTEIN.sh
-echo ""
-
 mkdir tmp_$PROTEIN
 cd tmp_$PROTEIN
 cp ../sample/$PROTEIN.pdb . 
 
+echo "Downloading the launching script ..."
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-myPresto/m/myPresto/sievgene/job_$PROTEIN.sh
 echo "Running the sievgene job for $PROTEIN..."
 sh job_$PROTEIN.sh
+echo ""
 
 echo "Copy the output files into the DOK artifact"
 mkdir -p /opt/artifact/results
+cd ..
 cp -r ./tmp_$PROTEIN /opt/artifact/results/
 ls /opt/artifact/results/
 
