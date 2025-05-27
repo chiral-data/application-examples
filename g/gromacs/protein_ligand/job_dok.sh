84efeb6b0a3375ba1366196ad924969570fddb3a
RongjunG <rongjungao0812@gmail.com>
2025-05-27 13:59:13 +0900

    fix: file path

--- g/gromacs/protein_ligand/job_dok.sh
+++ g/gromacs/protein_ligand/job_dok.sh
@@ -5,20 +5,20 @@
 
 echo "Downloading input files ..."
 
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/job.sh
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/job.sh
 
 mkdir app
 mkdir data
 
 cd app
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/app/workflow.py
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/app/workflow.py
 
 cd ../data
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/ions.pdb
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/ligand.gro
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/ligand.itp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/structure.pdb
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/workflow.yml
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/data/ions.pdb
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/data/ligand.gro
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/data/ligand.itp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/data/structure.pdb
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3-add-gromacs-py/g/gromacs/protein_ligand/data/workflow.yml
 cd ..
 
 echo ""
