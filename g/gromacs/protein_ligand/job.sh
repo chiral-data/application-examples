4beabfeac0e516f393381dbf7021986fc6c54c73
RongjunG <rongjungao0812@gmail.com>
2025-05-27 13:50:22 +0900

    add gromacs py job

---
+++ g/gromacs/protein_ligand/job.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -v /data:/data biobb_wf_protein-complex_md_setup
---
+++ g/gromacs/protein_ligand/job_dok.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#
+
+
+echo "Downloading input files ..."
+
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/job.sh
+
+mkdir app
+mkdir data
+
+cd app
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/app/workflow.py
+
+cd ../data
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/ions.pdb
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/ligand.gro
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/ligand.itp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/structure.pdb
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.3/g/gromacs/protein_ligand/data/workflow.yml
+cd ..
+
+echo ""
+
+echo "Running the job ..."
+sh job.sh
+
+echo "Copy the output files into the DOK artifact"
+mkdir -p /opt/artifact/gromacs_result
+cp ./*.* /opt/artifact/gromacs_result/
+ls /opt/artifact/gromacs_result/
