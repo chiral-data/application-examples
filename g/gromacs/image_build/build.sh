37afba95d834d1ed07caba6e7bd517b17bf13678
Qin Wan <rogerwq@gmail.com>
2025-05-27 10:34:34 +0900

    rename @common/run.sh to run_dok.sh

--- g/gromacs/image_build/build.sh
+++ g/gromacs/image_build/build.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 #
-#
-
-echo "--- Individual Arguments ---"
-echo "Running script: $0 ..." 
-echo "Job script to be downloaded: $1"
-filename=$(basename "$1")
-echo "Script to run: $filename"
-echo ""
 
 # --- Checking the number of arguments ---
 echo "--- Number of Arguments ---"
 echo "Total number of arguments received: $#"
 if [ "$#" -eq 0 ]; then
   echo "No arguments provided. Please provide some arguments when running the script."
-  echo "Usage: ./run.sh <url_for_script>  ..."
+  echo "Usage: ./build.sh nvidia|biobb  ..."
   exit
 elif [ "$#" -gt 1 ]; then
   echo "You provided 2 or more arguments."
   exit
 fi
+
+echo "Building container image with base image from $1"
 echo ""
 
-echo "Downloading the job script ..."
-wget $1
-echo ""
-
-echo "Running the job script ..."
-sh $filename
-echo ""
-
-
-
+mkdir -p $1 & cd$_
+cp ../../../../@common/run_dok.sh ./
+cp ../Dockerfile_$1 ./Dockerfile
+docker build -t gromacs_dok_$1 .
+cd ..
+rm -rf $1
