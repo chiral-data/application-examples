1e3484f32ae1328ec5e84f96018c83d1790eb946
RongjunG <rongjungao0812@gmail.com>
2025-06-06 14:30:17 +0900

    test: boltz antibody-antigen example

--- b/boltz/boltz_dok/job.sh
+++ b/boltz/boltz_dok/job.sh
@@ -6,18 +6,18 @@
 # Install required packages
 # pip install boltz -U
 
-echo "Downloading prot_no_msa.yaml ..."
-wget https://raw.githubusercontent.com/jwohlwend/boltz/refs/heads/main/examples/prot_no_msa.yaml
+echo "Downloading 4G6K.fasta ..."
+wget https://www.rcsb.org/fasta/entry/4G6K -O 4G6K.fasta
 
 # Run 
 echo "Run boltz calculation ..."
-python3 -m boltz.main predict prot_no_msa.yaml
+python3 -m boltz.main predict 4G6K.fasta
 
 # Create results directory
-mkdir -p /opt/artifact/boltz_results_prot_no_msa
-cp ./boltz_results_prot_no_msa/predictions/prot_no_msa/prot_no_msa_model_0.cif /opt/artifact/boltz_results_prot_no_msa/
-cp ./boltz_results_prot_no_msa/predictions/prot_no_msa/confidence_prot_no_msa_model_0.json /opt/artifact/boltz_results_prot_no_msa/
-cp ./boltz_results_prot_no_msa/predictions/prot_no_msa/plddt_prot_no_msa_model_0.npz /opt/artifact/boltz_results_prot_no_msa/
+mkdir -p /opt/artifact/boltz_results_4G6K
+cp ./boltz_results_4G6K/predictions/4G6K/4G6K_model_0.pdb /opt/artifact/boltz_results_4G6K/
+cp ./boltz_results_4G6K/predictions/4G6K/confidence_4G6K_model_0.json /opt/artifact/boltz_results_4G6K/
+cp ./boltz_results_4G6K/predictions/4G6K/plddt_4G6K_model_0.npz /opt/artifact/boltz_results_4G6K/
 
 echo "Prediction completed. Results are available in results folder."
-ls /opt/artifact/boltz_results_prot_no_msa/
+ls /opt/artifact/boltz_results_4G6K/
