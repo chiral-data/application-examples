c4181ff978e01afe247cff64ffaf77cc6cbee291
RongjunG <rongjungao0812@gmail.com>
2025-06-16 14:09:25 +0900

    update file links and README

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ This is a collections of applications examples which can be used in chiral produ
 
 | Name                  | Sakura DOK            |
 | ---                   | ---                   |
-| Boltz1                | :heavy_check_mark:    |
+| Boltz-2               | :heavy_check_mark:    |
 | Gromacs               | :heavy_check_mark:    |
 | HADDOCK3              | :heavy_check_mark:    |
 | LightDock             | :heavy_check_mark:    |
 | myPresto              | :construction:        |
 | Ollama                | :heavy_check_mark:    |
 | Quantum Expresso      | :construction:        |
+| TxGemma               | :heavy_check_mark:    |
 | Whisper               | :construction:        |
 
--- g/gromacs/antibody_antigen/job_5dk3.sh
+++ g/gromacs/antibody_antigen/job_5dk3.sh
@@ -3,14 +3,14 @@
 #
 
 echo "Downloading input files ..."
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4_gromacsAb/g/gromacs/antibody_antigen/job.sh
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4_gromacsAb/g/gromacs/antibody_antigen/ions.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4_gromacsAb/g/gromacs/antibody_antigen/md.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4_gromacsAb/g/gromacs/antibody_antigen/mdout.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4_gromacsAb/g/gromacs/antibody_antigen/em.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4_gromacsAb/g/gromacs/antibody_antigen/npt.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4_gromacsAb/g/gromacs/antibody_antigen/nvt.mdp
-wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4_gromacsAb/g/gromacs/antibody_antigen/5dk3-rs1-311_5k-100ns.pdb
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4/g/gromacs/antibody_antigen/job.sh
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4/g/gromacs/antibody_antigen/ions.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4/g/gromacs/antibody_antigen/md.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4/g/gromacs/antibody_antigen/mdout.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4/g/gromacs/antibody_antigen/em.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4/g/gromacs/antibody_antigen/npt.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4/g/gromacs/antibody_antigen/nvt.mdp
+wget https://raw.githubusercontent.com/chiral-data/application-examples/refs/heads/v0.2.4/g/gromacs/antibody_antigen/5dk3-rs1-311_5k-100ns.pdb
 echo ""
 
 echo "Running the job ..."
